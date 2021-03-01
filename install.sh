70a06fe3769d1d300d4a6b99754e922c01ae413b
tqfx <tqfx@qq.com>
2021-03-01 11:53:14 +0800

    Modify install

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 
 outfile=run.sh
+tmpfile=$(date +%Y%m%d%H%M%S)
 echo '#!'$(which bash) > $outfile
 echo 'cd '$(pwd) >> $outfile
 echo $(which python3)" bing.py $1 img" >> $outfile
 chmod +x $outfile
+crontab -l > $tmpfile
+echo "1 0 * * * $(pwd)/$outfile" >> $tmpfile
+crontab $tmpfile
+rm -f $tmpfile
+
