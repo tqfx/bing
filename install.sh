7620a22181805389e7cedff7f6154fee9d04444d
tqfx <tqfx@qq.com>
2021-03-01 13:42:05 +0800

    Update install

--- install.sh
+++ install.sh
@@ -5,8 +5,12 @@ echo '#!'$(which bash) > $outfile
 echo 'cd '$(pwd) >> $outfile
 echo $(which python3)" bing.py $1 img" >> $outfile
 chmod +x $outfile
+tasktxt='1 0 * * * '"$(pwd)/$outfile"
+tasktxt2='1 0 \* \* \* '"$(pwd)/$outfile"
 crontab -l > $tmpfile
-echo "1 0 * * * $(pwd)/$outfile" >> $tmpfile
+sed -i -e "s|$tasktxt2||g" -e '/^$/d' $tmpfile
+echo "$tasktxt" >> $tmpfile
 crontab $tmpfile
+cat $tmpfile
 rm -f $tmpfile
 
