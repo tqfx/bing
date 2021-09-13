559d179090d9f77afd3ba8f051ce5ae4a624e130
tqfx <tqfx@foxmail.com>
2021-09-13 10:35:22 +0800

    fix(install.sh): fix bing.py

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 
-outfile=run.sh
-tmpfile=$(date +%Y%m%d%H%M%S)
-echo '#!'$(which bash) > $outfile
-echo 'cd '$(pwd) >> $outfile
-echo $(which python3)" bing.py" >> $outfile
-chmod +x $outfile
+fileout=run.sh
+filetmp=$(date +%Y%m%d%H%M%S)
+echo '#!'$(which bash) > $fileout
+echo 'cd '$(pwd) >> $fileout
+echo $(which python3)" bing.py $1" >> $fileout
+chmod +x $fileout
 
-tasktxt='1 0 * * * '"$(pwd)/$outfile"
-tasktxt2='1 0 \* \* \* '"$(pwd)/$outfile"
-crontab -l > $tmpfile 2> /dev/null
-sed -i -e "s|$tasktxt2||g" -e '/^$/d' $tmpfile
-echo "$tasktxt" >> $tmpfile
-crontab $tmpfile
-rm -f $tmpfile
+tasktxt='1 0 * * * '"$(pwd)/$fileout"
+tasktxt2='1 0 \* \* \* '"$(pwd)/$fileout"
+crontab -l > $filetmp 2> /dev/null
+sed -i -e "s|$tasktxt2||g" -e '/^$/d' $filetmp
+echo "$tasktxt" >> $filetmp
+crontab $filetmp
+rm -f $filetmp
 crontab -l
