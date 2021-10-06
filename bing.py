b625f666d84ec7802430e23c82e3ca1e39a111fb
tqfx <tqfx@foxmail.com>
2021-10-06 20:55:26 +0800

    fix(bing.py): certificate verify failed

--- bing.py
+++ bing.py
@@ -2,11 +2,13 @@
 # -*- coding : utf-8 -*-
 import os
 import sys
+import ssl
 import json
 import urllib.request
 import urllib.parse
 import urllib.error
 
+ssl._create_default_https_context = ssl._create_unverified_context
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"
 }
